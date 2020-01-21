8498d06eda839160f75caea6c9255b382bbada97
Federico Stella <federico.stella95@gmail.com>
2020-01-21 02:59:09 +0100

    Automatically saving midi files

--- src/program/CmdLineMain.java
+++ src/program/CmdLineMain.java
@@ -1,5 +1,7 @@
 package program;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,6 +24,8 @@ public class CmdLineMain {
 	private static Track track;
 	private static Sequence sequence;
 	
+	private static String baseFolder = java.time.LocalDateTime.now().toString();
+	
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in); 
 		
@@ -31,8 +35,17 @@ public class CmdLineMain {
 		GeneticAlgorithm alg = new GeneticAlgorithm();
 		int generation = 1;
 		
+		//Create base folder
+		File f = new File(baseFolder);
+		if (!f.mkdir()) {
+			System.out.println("Error in folder creation.");
+			System.exit(1);
+		}
+		
+		
 		do {
 			System.out.println("\nGeneration " + generation);
+			savePopulation(population, generation);
 			showPopulation(population);
 			System.out.println("\nChoose the measure to play and set fitness. -1 to play all.\nType 'G' to generate a new epoch, 'E' to exit");
 			String line = in.nextLine();
@@ -90,6 +103,50 @@ public class CmdLineMain {
 		}
 	}
 	
+	public static void savePopulation(List<Measure> population, int generation) {
+		try {
+			
+			String path = baseFolder + File.separatorChar + "gen_" + generation;
+			File folder = new File(path);
+			if (!folder.mkdir()) {
+				System.out.println("Couldn't create folder for generation " + generation);
+				return;
+			}
+			
+			
+			for (int i=0; i<population.size(); i++) {
+				Measure m = population.get(i);
+				
+				sequencer = MidiSystem.getSequencer();
+				sequencer.open(); 
+				
+				sequence = new Sequence(Sequence.PPQ, 2);
+				track = sequence.createTrack(); 
+				
+				ShortMessage sm = new ShortMessage(192,1,1,0); //Set instrument (the third number, 1, is Acoustic Piano)
+				track.add(new MidiEvent(sm, 0));
+				
+				m.addToTrack(track);
+				
+				track.add(new MidiEvent(new ShortMessage(128,1,50,100), m.getLength()));
+		
+				sequencer.setSequence(sequence); 
+				
+				sequencer.setTempoInBPM(120); 
+				
+				
+				
+				File f = new File(path + File.separatorChar + "measure_"+i+".mid");
+				MidiSystem.write(sequence,1,f);
+			}
+			
+		} catch (InvalidMidiDataException | MidiUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
 	
 	public static void playMeasure(Measure m) {
 		try {
@@ -98,6 +155,10 @@ public class CmdLineMain {
 			
 			sequence = new Sequence(Sequence.PPQ, 2);
 			track = sequence.createTrack(); 
+			
+			
+			ShortMessage sm = new ShortMessage(192,1,1,0); //Set instrument (the third number, 1, is Acoustic Piano)
+			track.add(new MidiEvent(sm, 0));
 			
 			m.addToTrack(track);
 			
